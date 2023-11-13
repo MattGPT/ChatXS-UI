9c08a73b712aaf7cdc93c350c8361d2138721d28
MattGPT <132846030+MattGPT@users.noreply.github.com>
2023-11-13 11:36:28 +0000

    Update Temperature.tsx

--- components/Chat/Temperature.tsx
+++ components/Chat/Temperature.tsx
@@ -36,7 +36,7 @@ export const TemperatureSlider: FC<Props> = ({
       </label>
       <span className="text-[12px] text-black/50 dark:text-white/50 text-sm">
         {t(
-          'Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic.',
+          'Higher values like 1.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic.',
         )}
       </span>
       <span className="mt-2 mb-1 text-center text-neutral-900 dark:text-neutral-100">
@@ -46,7 +46,7 @@ export const TemperatureSlider: FC<Props> = ({
         className="cursor-pointer"
         type="range"
         min={0}
-        max={1}
+        max={2}
         step={0.1}
         value={temperature}
         onChange={handleChange}
